f27f73ff20fb6ad70c60e93b4db5e6af6d582147
Isabelly Faria <isabelly.faria06@gmail.com>
2025-09-25 21:38:43 -0300

    Inicia Funções dos Pontos

--- desenhador.js
+++ desenhador.js
@@ -3,7 +3,7 @@ let canvas = document.getElementById("crocheCanvas");
 let ctx = canvas.getContext("2d"); // contexto 2D
 function circuloMagico(){
   ctx.beginPath();
-  ctx.arc(150, 300, 4, 0, 2 * Math.PI); // x, y, raio, ângulo inicial, final
+  ctx.arc(200, 300, 4, 0, 2 * Math.PI); // x, y, raio, ângulo inicial, final
   ctx.stroke(); // borda
 }
 circuloMagico()
@@ -16,10 +16,29 @@ function organizaFila(receita){
     });
     return instrucoes_receita
 }
+function pontoBaixo(){
+  ctx.beginPath();
+  //FAZ TRAÇO HORIZINTAL
+  ctx.moveTo(500, 300);       // Inicia um ponto
+  ctx.lineTo(515, 300);       // Desenha uma linha
+  ctx.closePath();          // Fecha o caminho (opcional)
+  ctx.stroke();             // Desenha o contorno
+  ctx.fill();               // Preenche a forma
+  //FAZ TRAÇO VERTICAL
+  ctx.beginPath();
+  ctx.moveTo(508, 307);       // Inicia um ponto
+  ctx.lineTo(508, 293);       // Desenha uma linha
+  ctx.closePath();          // Fecha o caminho (opcional)
+  ctx.stroke();             // Desenha o contorno
+  ctx.fill();               // Preenche a forma
+}
+pontoBaixo()
 function organizaArvore(receita){
   
 }
-
+function adicionaPontoCircunferencia(Cx,Cy,raio,pontos){
+  //
+}
 formulario.addEventListener('submit', (event) => {
   event.preventDefault(); // impede o envio tradicional
   // Pega valor da receita
